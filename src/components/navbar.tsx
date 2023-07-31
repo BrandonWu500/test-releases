fd9f44e4ab26082c275bcaafe2a8c4ea8e9b262a
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-07-31 15:58:50 -0400

    feat: add search to navbar (#16)

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,12 @@
 import Logo from "@/components/logo";
+import Search from "@/components/search";
 
 type Props = {};
 const Navbar = (props: Props) => {
   return (
     <div>
       <Logo />
+      <Search />
     </div>
   );
 };
