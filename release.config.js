2f182dfc450f591b582545152988c6bc3f8ab087
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-07-31 15:36:36 -0400

    chore: add changelog as asset (#13)

--- release.config.js
+++ release.config.js
@@ -6,6 +6,7 @@ const config = {
     [
       "@semantic-release/git",
       {
+        assets: "CHANGELOG.md",
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
